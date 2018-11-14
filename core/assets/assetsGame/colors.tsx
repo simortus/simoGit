ce78868210d93112670d6ee51e43f9a1fd4f9656
cilyes18 <cilyes18@student.aau.dk>
2018-11-14 11:34:23 +0100

    assets added

---
+++ core/assets/assetsGame/colors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="colors" tilewidth="64" tileheight="64" spacing="10" margin="10" tilecount="2183" columns="59">
+ <image source="D:/Descarcari/kisspng-dot-circles-space-roos-color-clip-art-colors-5abd04711fd4b6.3610536415223368811304.png" trans="ff00ff" width="4417" height="2800"/>
+</tileset>
