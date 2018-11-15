c1bd47ad2ddd9eec83c279f12c18e452d0727251
dimos <dimos_samos@hotmail.com>
2018-11-15 14:43:14 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	core/src/com/mygdx/game/TileBoard2.java

--- core/src/com/mygdx/game/TileBoard3.java
+++ core/src/com/mygdx/game/TileBoard3.java
@@ -1,8 +1,156 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.*;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 
-public class Tileboard3 extends ApplicationAdapter
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.mygdx.game.supp.Dice;
+
+public class TileBoard3 extends ApplicationAdapter
 {
-    
+    private static Stage stage;
+
+    private OrthographicCamera camera;
+    private FillViewport viewport;
+
+    private static TiledMap tiledMap;
+    private OrthogonalTiledMapRenderer renderer;
+
+    private static MapLayers layerList;
+    private static MapLayer layer;
+    private static MapObjects objList;
+    private static MapObject obj;
+    private static MapProperties objProperties;
+
+
+    private Texture texture;
+    private static Image pawn;
+
+    int w;
+    int h;
+
+
+    @Override
+    public void create()
+    {
+
+        // Dimensions of TiledMap
+        w = 12800;
+        h = 7680;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w, h);
+        camera.update();
+        viewport = new FillViewport(w, h, camera);
+
+        stage = new Stage(viewport);
+
+        tiledMap = new TmxMapLoader().load("board640C.tmx");
+        renderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+        texture = new Texture(Gdx.files.internal("icon.png"));
+
+        // Creating a pawn in a starting position
+        pawn = new Image(texture);
+        pawn.setSize(texture.getWidth() * .5f, texture.getHeight() * .5f);
+        pawn.setPosition(160, 96);
+        stage.addActor(pawn);
+
+
+
+//********************************************************************************************
+       /*
+        // Getting an Array with the layers of our tiledMap
+        layerList = tiledMap.getLayers();
+        // Getting a specific layer from the layerList
+        layer = layerList.get("Tiles");    // Equal to .get(3)
+        // Getting a property of the layer (e.g name)
+        String layerProperty = layer.getName();
+
+        // Getting an Array of the layer's MapObjects
+        MapObjects objectList = layer.getObjects();
+        // Checking how many MapObjects the layer has
+        int countObjects = objectList.getCount();
+        // Getting a MapObject, in our case a created rectangle-tile
+        MapObject obg = objectList.get("tile004");
+
+        // Getting an Array of the MapObject properties
+        MapProperties obgProperties = obg.getProperties();
+        // Getting a Value of the tile's MapProperties using its key
+        Object obgLecture = obgProperties.get("lecture");
+
+        System.out.printf("layer: %s%nobjects no.: %d%nTile Name: %s%nLecture: %s%nPositioned: %s x, %s y",
+                 layerProperty, countObjects, obg.getName(), obgLecture, obgProperties.get("x"), obgProperties.get("y"));
+       */
+//********************************************************************************************
+        /*
+        MoveToAction action = new MoveToAction();
+        action.setPosition((Float) obgProperties.get("x"), (Float) obgProperties.get("y"));
+        action.setDuration(4);
+        action.setInterpolation(Interpolation.smooth);
+        pawn.addAction(action);
+        stage.addActor(pawn);
+        */
+//********************************************************************************************
+
+    }
+
+    public static void movePawn(int diceSum)
+    {
+        layerList = tiledMap.getLayers();
+        layer = layerList.get("Tiles");
+        objList = layer.getObjects();
+        obj = objList.get("tile" + Integer.toString(diceSum));
+        objProperties = obj.getProperties();
+
+        MoveToAction action = new MoveToAction();
+        action.setPosition((Float) objProperties.get("x"), (Float) objProperties.get("y"));
+        action.setDuration(1);
+        action.setInterpolation(Interpolation.smooth);
+        pawn.addAction(action);
+        stage.addActor(pawn);
+    }
+
+
+    @Override
+    public void render()
+    {
+        Gdx.gl.glClearColor(.9038f, .9038f, .9038f, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+//        renderer.setView((OrthographicCamera) viewport.getCamera());
+        renderer.setView(camera);
+        renderer.render();
+
+
+        Dice.rollAndMove();
+
+        // Instead of batch and sprites
+        stage.act();
+        stage.draw();
+    }
+
+
+    @Override
+    public void dispose()
+    {
+        tiledMap.dispose();
+        stage.dispose();
+    }
 }
--- desktop/src/com/mygdx/game/desktop/LaunchTileBoard3.java
+++ desktop/src/com/mygdx/game/desktop/LaunchTileBoard3.java
@@ -1,4 +1,20 @@
 package com.mygdx.game.desktop;
 
-public class LaunchTileBoard3 {
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.TileBoard3;
+
+public class LaunchTileBoard3
+{
+    public static void main (String[] args)
+    {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.width = 900;
+        config.height = 600;
+//        config.backgroundFPS = 60;
+//        config.foregroundFPS = 60;
+
+        new LwjglApplication(new TileBoard3(), config);
+    }
 }
